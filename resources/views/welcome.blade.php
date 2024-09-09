9ed31165e386493123e908b722a950e1fba7a7f6
Alamin <alamin@duck.com>
2024-09-09 11:33:04 +0600

    modified title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome to Our Todo App</title>
+    <title>Todo App</title>
     <!-- Include Tailwind CSS from CDN for simplicity -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
